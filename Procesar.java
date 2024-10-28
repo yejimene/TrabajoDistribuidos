d3e6878a2c74be80b46a0f53b77e2c097c290400
yeray <yerayjz04@gmail.com>
2024-10-28 22:28:22 +0100

    Server y procesar echo, falta la interfaz

--- Procesar.java
+++ Procesar.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +6,6 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
-import java.util.concurrent.Callable;
 
 public class Procesar implements Runnable {
     Socket s;
