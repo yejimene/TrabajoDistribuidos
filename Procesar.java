65f1dc9ed498a17d94ba91a4b45b636554e11df9
yeray <yerayjz04@gmail.com>
2024-10-29 19:54:14 +0100

    Mejorado

--- Procesar.java
+++ Procesar.java
@@ -1,16 +1,22 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Procesar implements Runnable {
     Socket s;
-    private static Vector<String> ButacasCompradas= new Vector<>();
+    private static ConcurrentHashMap<String, Vector<String>> Cine = new ConcurrentHashMap<String, Vector<String>>();
     private List<String> lista= new ArrayList<>();
+    private String clave;
     public Procesar(Socket s) {
         this.s= s;
     }
@@ -18,11 +24,12 @@ public class Procesar implements Runnable {
         try(BufferedReader in =new BufferedReader(new InputStreamReader(s.getInputStream())) ;
             DataOutputStream out= new DataOutputStream(s.getOutputStream())){
             String id;
-            while((id=in.readLine())!=null) {
+            clave= in.readLine();
+            enviarAsientosOcupados(clave);
+            while(!(id=in.readLine()).equals("ACABADO")) {
                 lista.add(id);
             }
-            if(estaDisponible(lista)) {
-                ComprarButacas(lista);
+            if(comprar(lista)) {
                 out.writeBoolean(true);
             }else {
                 out.writeBoolean(false);
@@ -38,20 +45,29 @@ public class Procesar implements Runnable {
         }
     }
 
-    public synchronized boolean estaDisponible(List<String> in) {
-        for(String id: in) {
-            if(ButacasCompradas.contains(id)) {
-                return false;
+    public synchronized boolean comprar(List<String> s) {
+        Vector<String> vector= Cine.get(clave);
+        for(String linea: vector) {
+            for(String s1: s) {
+                if(linea.equals(s1)) {
+                    return false;
+                }
             }
+        }
+        for(String s1: s) {
+            vector.add(s1);
+
         }
         return true;
     }
-    public synchronized void ComprarButacas(List<String> in)  {
-        for(String id: in) {
-            ButacasCompradas.add(id);
+    public void enviarAsientosOcupados(String clave) {
+        try(BufferedWriter out =new BufferedWriter(new OutputStreamWriter(s.getOutputStream()))){
+            for(String s : Cine.get(clave)) {
+                out.write(s+"\n");
+            }
+            out.flush();
+        }catch(IOException e) {
+            e.printStackTrace();
         }
     }
-
-
-
 }
